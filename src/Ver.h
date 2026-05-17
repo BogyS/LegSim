7678ed0b1fc7bf6f3f9f8bcbe9b32132c50480dd
Bogy <bogdan.sintoma@gmail.com>
2026-05-17 10:04:37 +0300

    - remove pio

--- src/Ver.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifndef _Ver_h_20260511
-#define _Ver_h_20260511
-
-const char Name[] = "Leg skel 03";   // Name of the device, shown in the web interface and mDNS
-const char Ver[] = "26.05.11.00";    // Version of the firmware
-
-#endif // _Ver_h_20260511
